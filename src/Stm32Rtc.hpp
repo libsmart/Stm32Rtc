ab961a6107c2fc4be1d806b9c39f6505743e2383
Roland Rusch <roland@rusch.lu>
2024-11-08 11:42:46 +0100

    #1 Add example

---
+++ src/Stm32Rtc.hpp
@@ -0,0 +1,10 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Roland Rusch, easy-smart solution GmbH <roland.rusch@easy-smart.ch>
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef LIBSMART_STM32RTC_STM32RTC_HPP
+#define LIBSMART_STM32RTC_STM32RTC_HPP
+
+
+#endif
---
+++ src/libsmart_config.dist.hpp
@@ -0,0 +1,13 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Roland Rusch, easy-smart solution GmbH <roland.rusch@easy-smart.ch>
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef LIBSMART_STM32RTC_LIBSMART_CONFIG_DIST_HPP
+#define LIBSMART_STM32RTC_LIBSMART_CONFIG_DIST_HPP
+
+#define LIBSMART_STM32RTC
+
+
+
+#endif
